032c4f251934df68a49f0c42b8e0fd0a2905f512
Vincent Tembo <vincenttembo@Vincents-MacBook-Pro.local>
2019-01-12 18:41:14 -0500

    Initial dataquest code upload

---
+++ exploratory-data-visualization/Bar Plots And Scatter Plots-144.py
@@ -0,0 +1,7 @@
+## 2. Introduction to the data ##
+
+import pandas as pd
+reviews = pd.read_csv('fandango_scores.csv')
+cols = ['FILM', 'RT_user_norm', 'Metacritic_user_nom', 'IMDB_norm', 'Fandango_Ratingvalue', 'Fandango_Stars']
+norm_reviews = reviews[cols]
+print(norm_reviews[:1])
--- python_programming-1/python1.md
+++
@@ -1,1 +0,0 @@
-# Python Progamming: Beginner Projects
